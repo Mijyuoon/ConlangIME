b2d843c445ac48eed7746d35a09a87610c5752c0
Mijyuoon <mijyuoon@gmail.com>
2025-04-26 15:21:08 +0300

    Added initial Mwadengrukay support.

---
+++ ConlangIME/InputMethods/MwadengRomV1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using ConlangIME.Languages;
+
+namespace ConlangIME.InputMethods
+{
+    [InputMethod(typeof(Mwadengrukay))]
+    public class MwadengRomV1 : IInputMethod
+    {
+        public string Name => "MwadengRomV1";
+
+        private const string LetterToken = "letr";
+        private const string PunctToken = "punc";
+
+        private const string MarkConsCluster = "mark.cc";
+        private const string MarkGeminate = "mark.gc";
+
+        private const string VowelA = "a";
+
+        private static readonly Dictionary<char, string> PunctMap = new()
+        {
+            { ',', "brk1" }, { '.', "brk2" }, { '-', "list" },
+            { '(', "par1lt" }, { '{', "par2lt" }, { '[', "numlt" },
+            { ')', "par1rt" }, { '}', "par2rt" }, { ']', "numrt" },
+        };
+
+        private static readonly Regex ScanRegex = new(
+            @"([ptčkbdžgfsšxzɣmnñŋwryl])|([aeiou])|([\,\.\-\(\)\[\]\{\}])|.",
+            RegexOptions.Compiled);
+
+        public IEnumerable<Token> Tokenize(string input)
+        {
+            var firstVowelA = true;
+            var lastConsonant = String.Empty;
+
+            foreach (Match rm in ScanRegex.Matches(input))
+            {
+                if (rm.Groups[1].Success)
+                {
+                    var tok = rm.Groups[1].Value;
+
+                    if (tok == lastConsonant)
+                    {
+                        yield return Token.Sub(MarkGeminate);
+                    }
+                    else
+                    {
+                        if (!String.IsNullOrEmpty(lastConsonant))
+                        {
+                            yield return Token.Sub(MarkConsCluster);
+                        }
+
+                        yield return Token.Sub($"{LetterToken}.{tok}");
+                    }
+
+                    firstVowelA = false;
+                    lastConsonant = tok;
+                }
+                else if (rm.Groups[2].Success)
+                {
+                    var tok = rm.Groups[2].Value;
+
+                    if (tok != VowelA || firstVowelA)
+                    {
+                        yield return Token.Sub($"{LetterToken}.{tok}");
+                    }
+
+                    firstVowelA = true;
+                    lastConsonant = String.Empty;
+                }
+                else if (rm.Groups[3].Success)
+                {
+                    var tok = PunctMap[rm.Groups[3].Value[0]];
+
+                    if (!String.IsNullOrEmpty(lastConsonant))
+                    {
+                        yield return Token.Sub(MarkConsCluster);
+                    }
+
+                    yield return Token.Sub($"{PunctToken}.{tok}");
+
+                    firstVowelA = true;
+                    lastConsonant = String.Empty;
+                }
+                else
+                {
+                    if (!String.IsNullOrEmpty(lastConsonant))
+                    {
+                        yield return Token.Sub(MarkConsCluster);
+                    }
+
+                    yield return Token.Raw(rm.Value);
+
+                    firstVowelA = true;
+                    lastConsonant = String.Empty;
+                }
+            }
+
+            if (!String.IsNullOrEmpty(lastConsonant))
+            {
+                yield return Token.Sub(MarkConsCluster);
+            }
+        }
+    }
+}
---
+++ ConlangIME/Languages/Mwadengrukay.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace ConlangIME.Languages
+{
+    [Language]
+    public class Mwadengrukay : ILanguage
+    {
+        static readonly FontFamily FontFamily = new(new Uri("pack://application:,,,/"), "./Fonts/#Mwadengrukay");
+
+        public string Name => "Mwadengrukay";
+        public FontFamily Font => FontFamily;
+        public double FontSize => 27.0;
+
+        private const int LetterBase = 0xE000;
+        private const int MarksBase = 0xE01B;
+        private const int PunctBase = 0xE020;
+
+        private static readonly Dictionary<char, int> LetterMap = Utils.IndexMap("ptčkbdžgfsšxzɣmnñŋwrylaeiou");
+
+        private static readonly Dictionary<string, int> MarksMap = Utils.IndexMap(new[]
+        {
+            null, null, null, "cc", "gc",
+        });
+
+        private static readonly Dictionary<string, int> PunctMap = Utils.IndexMap(new[]
+        {
+            "brk1", "brk2", "list", "numlt", "numrt", "par1lt", "par1rt", "par2lt", "par2rt",
+        });
+
+        public string Process(IEnumerable<Token> tokens)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var tk in tokens)
+            {
+                if (tk.IsSub)
+                {
+                    var par = tk.Value.Split('.');
+                    var (type, par1) = (par[0], par[1]);
+
+                    sb.Append(type switch
+                    {
+                        "letr" => (char)(LetterBase + LetterMap[par1[0]]),
+                        "mark" => (char)(MarksBase + MarksMap[par1]),
+                        "punc" => (char)(PunctBase + PunctMap[par1]),
+                        _ => throw new FormatException("invalid token type"),
+                    });
+                }
+                else
+                {
+                    sb.Append(tk.Value);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
